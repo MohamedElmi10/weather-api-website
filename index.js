b4b4a1a01b05ec0551fd65ee09d8b8793f81fda2
MohamedElmi10 <144214489+MohamedElmi10@users.noreply.github.com>
2024-04-22 19:19:03 +0200

    done displaying the place and date of the call

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const apiKey = "6c15264310d68da150f87264de5613b1"
-const city = document.getElementById("searchbarInputField").value
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${apiKey}`
+const d = new Date();
+const weekdays = ["sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const weekday = weekdays[d.getDay()]
+console.log(weekday)
+const dayOfTheMonth = d.getDate()
+console.log(dayOfTheMonth)
+const month = d.getMonth()
+console.log(month)
+const incrementMonth = month + 1
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const monthName = monthNames[month]
+console.log(monthName)
 
-fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Response was not ok")
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error => {
-        console.error("there was a problem with the fetch operation", error)
-    })
+const button = document.getElementById("buttonForSearch")
+
+function handleClick() {
+    const city = document.getElementById("searchbarInputField").value
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Response was not ok")
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            document.getElementById("showcaseGeneralInfo").innerHTML = `
+            <h2>${data.name}</h2>
+                <h4>${weekday}  ${dayOfTheMonth}  ${monthName} </h4>
+                <h5>09:00</h5>
+                <img src="pictures/Weather icons/cold-weather.png" alt="" id="weatherImage">
+                <h2>-11</h2>
+                <h5>Light snow</h5>
+            
+            `
+        })
+        .catch(error => {
+            console.error("there was a problem with the fetch operation", error)
+        })
+
+}
+
+button.addEventListener("click", handleClick)
